a7d396bb03753ccc0fdd229d0d0a56d8f88371e8
hackifiery <129475587+hackifiery@users.noreply.github.com>
2025-10-01 18:51:47 +0000

    fix pytest

--- test_sample.py
+++ test_sample.py
@@ -1,6 +1,11 @@
 from falcon import run_file
 import pytest
+import sys
+from io import StringIO
 
 def test_answer():
     with pytest.raises(SystemExit):
+        stdin_bak = sys.stdin
+        sys.stdin = StringIO("3")
         run_file("test.fa")
+        sys.stdin = stdin_bak
